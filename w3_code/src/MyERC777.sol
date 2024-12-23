df5ecc6af76193001d4e1e87e7ab1ce693c3f194
zhuozhi66666688 <zhuozhi666666@gmail.com>
2024-12-23 15:56:42 +0800

    init config project success

--- w3_code/src/MyERC777.sol
+++ w3_code/src/MyERC777.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.0;
 import "openzeppelin-contracts/contracts/token/ERC777/ERC777.sol";
 
 contract MyERC777 is ERC777 {
-    constructor() ERC777("MY777", "M777", new address[](0))
-    {
+    constructor() ERC777("MY777", "M777", new address[](0)) {
         _mint(msg.sender, 1000 * 10 ** 18, "", "");
     }
 }
