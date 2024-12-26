4e9961f41af70b5d19b8720a2488b4c443d1eabf
zhuozhi66666688 <zhuozhi666666@gmail.com>
2024-12-26 17:42:54 +0800

    hardhat

--- w3_code/src/MyERC20.sol
+++ w3_code/src/MyERC20.sol
@@ -4,12 +4,8 @@ pragma solidity ^0.8.0;
 
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-
 contract MyERC20 is ERC20 {
-
     constructor() ERC20(unicode"集训营二期", "CAMP2") {
         _mint(msg.sender, 10000 * 10 ** 18);
     }
-
-
 }
